91be55aa14e33f6b1bf4815376cb5720e3ea4520
Luan <luann.victor40@gmail.com>
2021-04-05 17:51:08 -0300

    Commit CRUD calculo de custo

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("elo.co.lvlzco_cds001.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("elo.co.lvlzco_cds001.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
